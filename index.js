7f9f5049df286359bbd0a0f0e01d9a7f1fedda38
Mohit Chhapru <mchhapru@horizon.csueastbay.edu>
2017-04-27 11:05:25 -0700

    'Update'

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/db', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 	  client.query('SELECT * FROM test_table', function(err, result) {
